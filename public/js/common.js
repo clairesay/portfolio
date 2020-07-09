e73f9a75dc33e47ddb10e0fb92297ddc2106004b
Claire <clairesay13@gmail.com>
2020-07-09 13:16:45 +1000

    progress bar progress :laughing:

--- public/js/common.js
+++ public/js/common.js
@@ -28,3 +28,50 @@ window.addEventListener('scroll', function headerShadow() {
         header.style.boxShadow = "0 4px 2px -2px transparent"
     }
 })
+
+//////////////////// PROJECT PROGRESS BAR ///////////////////////////
+
+const distanceArray = [];
+
+const caseStudyTitle = document.getElementsByTagName('h4');
+const progressStep = document.getElementsByClassName('progress-step');
+const progressStepLabel = document.getElementsByClassName('progress-step-label')
+
+for (var i = 0; i < caseStudyTitle.length; i ++) {
+    function offset(el) {
+        const elRect = el.getBoundingClientRect(),
+        scrollT = window.pageYOffset || document.documentElement.scrollTop;
+        return { top: elRect.top + scrollT }
+    }
+    const progressStepOffset = offset(caseStudyTitle[i]);
+    distanceArray.push(progressStepOffset.top);
+}  
+
+window.onscroll = function () {
+    for (var j = 0; j < caseStudyTitle.length; j ++) {
+        if ((window.scrollY + 100 > distanceArray[j]) && (window.scrollY + 100 < distanceArray[j + 1])) {
+            progressStep[j].style.backgroundColor = "white";
+            progressStep[j].style.transform = "scale(1.5)";
+            progressStepLabel[j].style.fontWeight = "700";
+            for (var k = 0; k < caseStudyTitle.length; k ++) {
+                if (k !== j) {
+                    if (progressStep[k] == undefined) {
+
+                    } else {
+                    progressStep[k].style.transform = "scale(1)";
+                    progressStep[k].style.backgroundColor = "#ffdd44";
+                    progressStepLabel[k].style.fontWeight = "400";
+                    }
+                }
+            }
+        }
+    }
+}
+
+// // example use
+// var div = document.querySelector('div');
+// var divOffset = offset(div);
+// console.log(divOffset.top);
+
+
+
