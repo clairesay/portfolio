c460fd69a195da2cdca3dfa4c98f03692300efb1
Claire <clairesay13@gmail.com>
2020-07-18 16:52:47 +1000

    successful changes to slidecards (finally)

--- public/js/project.js
+++ public/js/project.js
@@ -295,43 +295,53 @@ function buttonPosition(state) {
     }
 }
 
+// 
+
 var slideArticle = document.getElementsByClassName('slide-article');
 var slideCards = document.getElementsByClassName('slide-cards');
-
-
 var slideFunctionBaseNumber = 0;
 
-for (var k = 0; k < slideArticle.length; k ++ ) {
+// for (var m = 0; m < slideArticle.length; m ++) {
+//     slideCards[m].addEventListener('onscroll', slideCardFunction(m))
+// }
 
-}
 
-function slideCardFunction(m) {
-    var slideCardsDiv = slideCards[m].getElementsByTagName('div');
+function slideCardFunction(number) {
+    var slideCardsDiv = slideCards[number].getElementsByTagName('div');
     var slideWidth = slideCardsDiv[0].clientWidth;
-    // var next = slideArticle.getElementsByClassName('next');
-    // var previous = slideArticle.getElementsByClassName('previous')
-
-    if (slideCards[m].scrollLeft < 50) {
-        console.log('left')
-    } else if (slideCards[m].scrollLeft > (slideCardsDiv.length - 1)*slideWidth - 50) {
-        console.log('right')
-    } else {
-        console.log('inbetween')
-    }
+    var next = document.getElementsByClassName('next')[number];
+    var previous = document.getElementsByClassName('previous')[number];
+        if (slideCards[number].scrollLeft < 50) {
+            next.style.opacity = "1";
+            next.style.visibility = "visible";
+            previous.style.opacity = "0";
+            previous.style.visibility = "hidden";
+        } else if (slideCards[number].scrollLeft > (slideCardsDiv.length - 1)*slideWidth - 50) {
+            next.style.opacity = "0";
+            next.style.visibility = "hidden";
+            previous.style.opacity = "1";
+            previous.style.visibility = "visible";
+        } else {
+            next.style.opacity = "1";
+            next.style.visibility = "visible";
+            previous.style.opacity = "1";
+            previous.style.visibility = "visible";
+        }
 }
+
 
 
 function slideFunction(n, m) {
+    var slideCardsDiv = slideCards[m].getElementsByTagName('div');
+    var slideWidth = slideCardsDiv[0].clientWidth;
     slideFunctionBaseNumber += n;
     if (n > 0) {
-        slideCards[m].scrollBy(400, 0);
+        slideCards[m].scrollBy((slideWidth - 50), 0);
 
     } else if (n < 0) {
 
-        slideCards[m].scrollBy(-200, 0)
+        slideCards[m].scrollBy((-slideWidth + 50), 0)
     }
-
-
 }
 
 
